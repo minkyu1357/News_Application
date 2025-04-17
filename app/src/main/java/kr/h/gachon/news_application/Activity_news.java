63b74b75770655ee32ba06150c028e1fb271bec6
minkyu108 <101365948+minkyu108@users.noreply.github.com>
2025-04-17 23:12:41 +0900

    spinner

--- app/src/main/java/kr/h/gachon/news_application/Activity_news.java
+++ app/src/main/java/kr/h/gachon/news_application/Activity_news.java
@@ -5,13 +5,16 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 public class Activity_news extends AppCompatActivity {
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
     private RecyclerView.LayoutManager layoutManager;
     private String[] mDataset;
+    private String[] years={"2020","2021","2022","2023","2024","2025"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +33,13 @@ public class Activity_news extends AppCompatActivity {
         Button button1=(Button) findViewById(R.id.button1);
         Button button2=(Button) findViewById(R.id.button2);
         Button button3=(Button) findViewById(R.id.button3);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<>(
+                this, android.R.layout.simple_spinner_item, years);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter1);
+        spinner.setPrompt("날짜 선택");
 
 
     }
